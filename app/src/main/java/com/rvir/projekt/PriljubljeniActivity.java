4f6d87928d11ca826d07023c4efcc01e30bb8afe
marko1rose <marko1rose@gmail.com>
2017-06-16 02:58:34 +0200

    GG

---
+++ app/src/main/java/com/rvir/projekt/PriljubljeniActivity.java
@@ -0,0 +1,17 @@
+package com.rvir.projekt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marko on 6/16/2017.
+ */
+
+public class PriljubljeniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.priljubljeni);
+    }
+}
